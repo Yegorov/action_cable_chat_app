ac6098fb906907ecb5a770c3bb98ccc95ed93c6f
Yegorov <yegorov0725@yandex.ru>
2017-11-21 14:05:49 +0300

    Add message datetime

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -3,4 +3,8 @@ module MessagesHelper
   def markdown_to_html(text)
     Kramdown::Document.new(text, input: 'GFM').to_html
   end
+
+  def pretty_date(datetime)
+    datetime.strftime('%d/%m/%y %H:%M')
+  end
 end
